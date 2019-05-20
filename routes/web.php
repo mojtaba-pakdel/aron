32158e3a386498a20adf5eaffce4838207479187
mojtaba pakdel <pakdel8463@yahoo.com>
2019-05-20 06:53:37 -0700

    create article , slider, course,episode migration

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function () {
-    return 'test';
-});
+Route::get('/admin/panel', 'Admin/PanelController@index');
